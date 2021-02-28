55d426464aec9010d848ef2269d903a58f4ec275
kop3una <kirillalbert54@gmail.com>
2021-02-28 13:35:39 +0300

    VignerCipher without Ru and lowRegister

--- src/lab_1.java
+++ src/lab_1.java
@@ -4,13 +4,24 @@ import java.util.Scanner;
 public class lab_1 {
     public static void main (String [] args){
         Scanner in = new Scanner(System.in);
-        ColumnMethodCipher ColumnMethod = new ColumnMethodCipher();
-        ColumnMethod.setCipherText(in.nextLine());
-       // ColumnMethod.setOriginalText(in.nextLine());
-        ColumnMethod.setKey(in.nextLine());
-      //  ColumnMethod.coding();
-        ColumnMethod.uncoding();
-        ColumnMethod.show();
+
+        VigenerCipher Vigner = new VigenerCipher();
+       // Vigner.setOriginalText(in.nextLine());
+        Vigner.setCipherText(in.nextLine());
+        Vigner.setKey(in.nextLine());
+     //   Vigner.coding();
+        Vigner.uncoding();
+        Vigner.show();
+   //     Vigner.showTableEn();
+   //     Vigner.showTableRu();
+
+//        ColumnMethodCipher ColumnMethod = new ColumnMethodCipher();
+//        ColumnMethod.setCipherText(in.nextLine());
+//       // ColumnMethod.setOriginalText(in.nextLine());
+//        ColumnMethod.setKey(in.nextLine());
+//      //  ColumnMethod.coding();
+//        ColumnMethod.uncoding();
+//        ColumnMethod.show();
 
 //        RailFenceCipher RailFence = new RailFenceCipher();
 //        System.out.print("Введите Исходный текст: ");
